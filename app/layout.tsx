3e51c5482eca933561fb0e9e993dc8765e84b041
praditus343 <egidanuarta17@gmail.com>
2025-06-06 17:20:16 +0700

    Add favicon images and site web manifest
    - Added favicon-16x16.png and favicon-32x32.png images to the project.
    - Introduced favicon.ico file containing the main favicon.
    - Created site.webmanifest for web app configuration with icons and color settings.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,26 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Panti Asuhan Pangrekso Dalem Bethlehem",
   description: "Website resmi Panti Asuhan Pangrekso Dalem Bethlehem",
+  icons: {
+    icon: [
+      { url: "/ico/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/ico/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: "/ico/favicon.ico",
+    apple: "/ico/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/ico/android-chrome-192x192.png",
+        sizes: "192x192",
+      },
+      {
+        rel: "icon", 
+        url: "/ico/android-chrome-512x512.png",
+        sizes: "512x512",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
